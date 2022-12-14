e095bb47a8078cbac3991b0a1024693579770f5f
Ruslan <RMingalievF@icloud.com>
2022-12-14 23:24:03 +0500

    Complete parse

---
+++ Teamplate/Networking/Links.swift
@@ -0,0 +1,11 @@
+//
+//  ApiManager.swift
+//  Teamplate
+//
+//  Created by Руслан Мингалиев on 14.12.2022.
+//
+
+enum Links: String {
+case cocktail = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+}
+
---
+++ Teamplate/Networking/NetworkDataPicker.swift
@@ -0,0 +1,30 @@
+//
+//  NetworkDataPicker.swift
+//  Teamplate
+//
+//  Created by Руслан Мингалиев on 14.12.2022.
+//
+
+import Foundation
+
+class ApiManager {
+    
+    static let shared = ApiManager()
+    
+     func cocktailRequest() {
+        
+        guard let url = URL(string:Links.cocktail.rawValue) else {return }
+        URLSession.shared.dataTask(with: url){ data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No ERROR")
+                return
+                }
+            do {
+                let coctail = try JSONDecoder().decode(Drinks.self, from: data)
+                print(coctail)
+            } catch let error {
+                print(error)
+            }
+        }.resume()
+    }
+}
