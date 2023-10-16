378f97d7db4d5c72973d34aa6d67fba80320f6dd
Ruslan <RMingalievF@icloud.com>
2023-10-16 22:53:07 +0500

    refactor: Update NetworkManager

---
+++ CocktailRecipes/Networking/NetworkCoctailRecipesManager.swift
@@ -0,0 +1,70 @@
+//
+//  NetworkCoctailRecipesManager.swift
+//  CocktailRecipes
+//
+//  Created by Руслан Мингалиев on 16.10.2023.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+	case invalidURL
+	case noData
+	case decodingError
+	case invalidStatusCode
+}
+
+class NetworkCoctailRecipesManager {
+	
+	static let shared = NetworkCoctailRecipesManager()
+	
+	let urlAPI = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+	
+	private init() {}
+	
+	func fetchCoctailRecipesInfo(from url: String, completion: @escaping (Result<[Coctails], NetworkError>) -> Void) {
+		guard let url = URL(string: urlAPI) else {
+			completion(.failure(.invalidURL))
+			return
+		}
+		
+		URLSession.shared.dataTask(with: url) { data, response, error in
+			guard let data = data else {
+				print(error?.localizedDescription ?? "No error description")
+				completion(.failure(.noData))
+				return
+			}
+			guard let httpResponse = response as? HTTPURLResponse,
+				  (200..<300).contains(httpResponse.statusCode) else {
+				completion(.failure(.invalidStatusCode))
+				return
+			}
+			do {
+				let coctailRecipes = try JSONDecoder().decode([Coctails].self, from: data)
+				DispatchQueue.main.async {
+					completion(.success(coctailRecipes))
+					print(data)
+				}
+			} catch {
+				completion(.failure(.decodingError))
+			}
+		}
+		.resume()
+	}
+	
+	func fetchImage(from url: String, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+		guard let url = URL(string: url) else {
+			completion(.failure(.invalidURL))
+			return
+		}
+		DispatchQueue.global().async {
+			guard let imageData = try? Data(contentsOf: url) else {
+				completion(.failure(.noData))
+				return
+			}
+			DispatchQueue.main.async {
+				completion(.success(imageData))
+			}
+		}
+	}
+}
